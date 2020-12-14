e67ca7f668fa235ae38e907d47538300028cbb6b
sovisrushain <sovisrushain@gmail.com>
2020-12-14 17:33:45 +0000

    Validate username.

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+/** Show input erroe message **/
+
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+/** Event Listner **/
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('submit');
+
+    if(username.value === ''){
+        showError(username, 'User name is required!')
+    }else{
+        showSuccess(username)
+    }
+
+})
